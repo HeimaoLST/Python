3c202b84e69a08d09ef0cb8a639682d565968626
HeimaoLST <3137636064@qq.com>
2023-02-21 16:36:55 +0800

    Latest!

--- ts.py
+++ ts.py
@@ -14,22 +14,11 @@ url = 'https://fanyi.baidu.com/sug'
 headers = {
 'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/110.0'
 	}
-print('HHHHHHHHH     HHHHHHHHH                      iiii                                                             LLLLLLLLLLL                SSSSSSSSSSSSSSS TTTTTTTTTTTTTTTTTTTTTTT')
-print('H:::::::H     H:::::::H                     i::::i                                                            L:::::::::L              SS:::::::::::::::ST:::::::::::::::::::::T')
-print('H:::::::H     H:::::::H                      iiii                                                             L:::::::::L             S:::::SSSSSS::::::ST:::::::::::::::::::::T')
-print('HH::::::H     H::::::HH                                                                                       LL:::::::LL             S:::::S     SSSSSSST:::::TT:::::::TT:::::T')
-print('  H:::::H     H:::::H      eeeeeeeeeeee    iiiiiii    mmmmmmm    mmmmmmm     aaaaaaaaaaaaa      ooooooooooo     L:::::L               S:::::S            TTTTTT  T:::::T  TTTTTT')
-print('  H:::::H     H:::::H    ee::::::::::::ee  i:::::i  mm:::::::m  m:::::::mm   a::::::::::::a   oo:::::::::::oo   L:::::L               S:::::S                    T:::::T        ')
-print('  H::::::HHHHH::::::H   e::::::eeeee:::::ee i::::i m::::::::::mm::::::::::m  aaaaaaaaa:::::a o:::::::::::::::o  L:::::L                S::::SSSS                 T:::::T        ')
-print('  H:::::::::::::::::H  e::::::e     e:::::e i::::i m::::::::::::::::::::::m           a::::a o:::::ooooo:::::o  L:::::L                 SS::::::SSSSS            T:::::T        ')
-print('  H:::::::::::::::::H  e:::::::eeeee::::::e i::::i m:::::mmm::::::mmm:::::m    aaaaaaa:::::a o::::o     o::::o  L:::::L                   SSS::::::::SS          T:::::T        ')
-print('  H::::::HHHHH::::::H  e:::::::::::::::::e  i::::i m::::m   m::::m   m::::m  aa::::::::::::a o::::o     o::::o  L:::::L                      SSSSSS::::S         T:::::T        ')
-print('  H:::::H     H:::::H  e::::::eeeeeeeeeee   i::::i m::::m   m::::m   m::::m a::::aaaa::::::a o::::o     o::::o  L:::::L                           S:::::S        T:::::T        ')
-print('  H:::::H     H:::::H  e:::::::e            i::::i m::::m   m::::m   m::::ma::::a    a:::::a o::::o     o::::o  L:::::L         LLLLLL            S:::::S        T:::::T        ')
-print('HH::::::H     H::::::HHe::::::::e          i::::::im::::m   m::::m   m::::ma::::a    a:::::a o:::::ooooo:::::oLL:::::::LLLLLLLLL:::::LSSSSSSS     S:::::S      TT:::::::TT      ')
-print('H:::::::H     H:::::::H e::::::::eeeeeeee  i::::::im::::m   m::::m   m::::ma:::::aaaa::::::a o:::::::::::::::oL::::::::::::::::::::::LS::::::SSSSSS:::::S      T:::::::::T      ')
-print('H:::::::H     H:::::::H  ee:::::::::::::e  i::::::im::::m   m::::m   m::::m a::::::::::aa:::a oo:::::::::::oo L::::::::::::::::::::::LS:::::::::::::::SS       T:::::::::T      ')
-print('HHHHHHHHH     HHHHHHHHH    eeeeeeeeeeeeee  iiiiiiiimmmmmm   mmmmmm   mmmmmm  aaaaaaaaaa  aaaa   ooooooooooo   LLLLLLLLLLLLLLLLLLLLLLLL SSSSSSSSSSSSSSS         TTTTTTTTTTT      ')
+print('verson:0.1')
+print("欢迎使用本英->汉词典，作者为HeimaoLST")
+print('遇到bug的话不用通知我，你们说的其实我都知道，只是不想改罢了(')
+print('PS：代码开源了，知道怎么解决的话最好帮我修一下')
+print('未来它可能会更新，希望它能帮到你')
 while True:
 
 	Date = {
